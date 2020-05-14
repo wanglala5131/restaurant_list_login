a26a891d2e3258c4f7626721bfeaeaa30be34623
Wanglala <sue4100035045@gmail.com>
2020-05-14 13:10:32 +0800

    feat: add restaurant seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurants')
+const restaurantList = require('./restaurant.json').results
+
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb connected')
+  for (let i = 0; i < restaurantList.length; i++) {
+    Restaurant.create({
+      name: restaurantList[i].name,
+      name_en: restaurantList[i].name_en,
+      category: restaurantList[i].category,
+      image: restaurantList[i].image,
+      location: restaurantList[i].location,
+      phone: restaurantList[i].phone,
+      google_map: restaurantList[i].google_map,
+      rating: restaurantList[i].rating,
+      description: restaurantList[i].description,
+    })
+  }
+})
