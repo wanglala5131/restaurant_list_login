9c4275ad4832e08f6156b14830b45235a7c5c519
Wanglala <sue4100035045@gmail.com>
2020-05-14 10:53:39 +0800

    feat: add html and stylesheets

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
