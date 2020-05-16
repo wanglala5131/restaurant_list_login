e9b08da567157f7921f2e1839de20b0937ab8408
Wanglala <sue4100035045@gmail.com>
2020-05-16 15:14:24 +0800

    refactor: simplify app.js DELETE code

--- app.js
+++ app.js
@@ -92,16 +92,16 @@ app.post('/restaurant/delete/:id', (req, res) => {
     .catch(err => console.log(err))
 })
 
-//搜尋
+//搜尋，可搜尋英文名、中文名和類型
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
+  const keyword = req.query.keyword.toLowerCase()
   Restaurant.find()
     .lean()
     .then(restaurants => {
       return restaurants.filter(restaurant =>
-        restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-        || restaurant.name_en.toLowerCase().includes(keyword.toLowerCase())
-        || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+        restaurant.name.toLowerCase().includes(keyword)
+        || restaurant.name_en.toLowerCase().includes(keyword)
+        || restaurant.category.toLowerCase().includes(keyword)
       )
     })
     .then(restaurants => res.render('index', { restaurants, keyword: keyword }))
