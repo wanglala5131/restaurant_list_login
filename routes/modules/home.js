cea72ef9ee310dfe4fa7544b208b643284bc9377
Wanglala <sue4100035045@gmail.com>
2020-06-06 02:34:19 +0800

    feat: relations between User and restaurant

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,7 +5,8 @@ const Restaurant = require('../../models/restaurants')
 
 //首頁
 router.get('/', (req, res) => {
-  Restaurant.find()
+  const userId = req.user._id
+  Restaurant.find({ userId })
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(err => console.log(err))
--- routes/modules/restaurant.js
+++ routes/modules/restaurant.js
@@ -5,8 +5,9 @@ const Restaurant = require('../../models/restaurants')
 
 //詳細頁
 router.get('/detail/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurant => res.render('show', { restaurant }))
     .catch(err => console.log(err))
@@ -17,30 +18,33 @@ router.get('/create', (req, res) => {
   res.render('create')
 })
 router.post('/', (req, res) => {
+  const userId = req.user._id
   let { name, name_en, category, phone, image, location, rating, google_map, description } = req.body
   if (!image.length) {
     image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/No_image_3x4.svg/640px-No_image_3x4.svg.png'
   }
-  return Restaurant.create({ name, name_en, category, phone, image, location, rating, google_map, description })
+  return Restaurant.create({ name, name_en, category, phone, image, location, rating, google_map, description, userId })
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 })
 
 //編輯店家資料
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurant => res.render('edit', { restaurant }))
     .catch(err => console.log(err))
 })
 router.put('/:id', (req, res) => {
   let { name, name_en, category, phone, image, location, rating, google_map, description } = req.body
-  const id = req.params.id
+  const userId = req.user._id
+  const _id = req.params.id
   if (!image.length) {
     image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/No_image_3x4.svg/640px-No_image_3x4.svg.png'
   }
-  return Restaurant.findById(id)
+  return Restaurant.findOne({ _id, userId })
     .then(restaurant => {
       restaurant.name = name
       restaurant.name_en = name_en
@@ -53,14 +57,15 @@ router.put('/:id', (req, res) => {
       restaurant.description = description
       return restaurant.save()
     })
-    .then(() => res.redirect(`/restaurant/detail/${id}`))
+    .then(() => res.redirect(`/restaurant/detail/${_id}`))
     .catch(err => console.log(err))
 })
 
 //刪除店家資料
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .then(restaurant => restaurant.remove())
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
